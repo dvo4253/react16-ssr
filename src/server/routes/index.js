0364c8f1778f1a3ddf8062b58242695e9401abad
Nathan Dvorachek <dvo4253.miscaccounts@gmail.com>
2018-07-27 23:40:19 -0500

    Added health route

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,9 +1,10 @@
 import express from 'express';
+import health from './health';
 import fallback from './fallback';
 import root from './root';
 
 const router = express.Router();
-
+router.use('/health', health);
 router.use('/', root);
 router.use('*', fallback);
 
