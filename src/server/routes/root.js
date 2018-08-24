de3ca37934f40b1d989f6e5479bc090041f74cdb
Nathan Dvorachek <8908406+dvo4253@users.noreply.github.com>
2018-08-24 01:00:48 -0500

    Removed console log

--- src/server/routes/root.js
+++ src/server/routes/root.js
@@ -7,7 +7,6 @@ const viewRouter = express.Router();
 
 viewRouter.route('/').get((req, res) => {
 	const jsxString = reactDomServer.renderToString(<App />);
-	console.log('JSX: ', jsxString);
 	res.render('output', { jsxString });
 });
 
